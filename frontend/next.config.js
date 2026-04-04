65999d0db96c01f716aba9645614bdb3db0a72a8
rpankivroma <pankiv.roma@gmail.com>
2026-04-04 16:28:31 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         destination: 'http://127.0.0.1:8000/api/:path*',
       },
       {
+        source: '/static/:path*',
+        destination: 'http://127.0.0.1:8000/static/:path*',
+      },
+      {
         source: '/db-status',
         destination: 'http://127.0.0.1:8000/db-status',
       },
